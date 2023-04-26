5014a46c675654357756519eca675894626520d6
Daniel Hjartland <squashical@gmail.com>
2023-04-26 22:47:56 +0200

    Add project configuration to gitignore and fix styling on 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <>
-      <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <main className="grid min-h-screen place-items-center px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-xl font-semibold text-sky-600">404</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-100 sm:text-5xl">
