26cb2b50a6909afb03d66eed8f8ec9bbef7620c8
Daniel Hjartland <squashical@gmail.com>
2023-04-29 09:34:09 +0200

    Remove unused InView component

--- src/components/landing-page/Intro.tsx
+++ src/components/landing-page/Intro.tsx
@@ -1,73 +1,64 @@
-'use client'
-import Image from 'next/image';
-import { InView, useInView } from 'react-intersection-observer';
+"use client";
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
 
-import profilePicUnsaturated from 'public/images/profile.jpg';
-import profilePicColor from 'public/images/profile_colour.jpeg';
+import profilePicUnsaturated from "public/images/profile.jpg";
 
 const Intro = () => {
   const { ref, inView } = useInView({
     root: null,
-    rootMargin: '0px',
+    rootMargin: "0px",
     threshold: 0.5,
   });
 
   return (
-    <InView>
-      <section
-        ref={ref}
+    <section
+      ref={ref}
+      className={`${
+        inView ? "opacity-100" : "opacity-0"
+      } max-w-screen flex max-h-screen min-h-screen transform snap-center flex-col items-center justify-center gap-4`}
+    >
+      <div
         className={`${
-          inView ? 'opacity-100' : 'opacity-0'
-        } max-w-screen flex max-h-screen min-h-screen transform snap-center flex-col items-center justify-center gap-4`}
+          inView ? "translate-0" : "-translate-x-[25vw] opacity-0"
+        } relative aspect-square w-48 transform overflow-hidden rounded-full duration-[1200ms] ease-out`}
       >
-        <div
-          className={`${
-            inView ? 'translate-0' : '-translate-x-[25vw] opacity-0'
-          } group relative aspect-square w-48 transform overflow-hidden rounded-3xl duration-[1200ms] ease-out`}
-        >
-          <Image
-            src={profilePicUnsaturated}
-            alt={'Profile picture'}
-            className="opacity-100 absolute transition duration-500 group-hover:opacity-0"
-          />
-          <Image
-            src={profilePicColor}
-            alt={'Profile picture'}
-            className="opacity-0 absolute transition duration-500 group-hover:opacity-100"
-          />
-          
-        </div>
+        <Image
+          src={profilePicUnsaturated}
+          alt={"Profile picture"}
+          className="absolute"
+        />
+      </div>
 
-        <div
-          className={`${
-            inView ? 'translate-0 opacity-100' : '-translate-x-[25vw] opacity-0'
-          } transform text-center text-4xl font-bold duration-[1200ms] ease-[cubic-bezier(0.45,-0.45,0.25,1.75)] sm:text-5xl`}
-        >
-          <span>
-            Fullstack-
-            <span className="bg-gradient-to-br from-sky-300 to-blue-500 bg-clip-text text-transparent">
-              utvikler
-            </span>
-          </span>
-          <br />
-          med <span className="text-sky-300">full</span>{' '}
+      <div
+        className={`${
+          inView ? "translate-0 opacity-100" : "-translate-x-[25vw] opacity-0"
+        } transform text-center text-4xl font-bold duration-[1200ms] ease-[cubic-bezier(0.45,-0.45,0.25,1.75)] sm:text-5xl`}
+      >
+        <span>
+          Fullstack-
           <span className="bg-gradient-to-br from-sky-300 to-blue-500 bg-clip-text text-transparent">
-            kontroll.
+            utvikler
           </span>
-        </div>
-        <div
-          className={`${
-            inView ? 'translate-0 opacity-100' : 'translate-x-[25vw] opacity-0'
-          } mt-8 transform whitespace-nowrap text-center text-lg font-thin italic duration-[1200ms] ease-[cubic-bezier(0,0,0.5,1.5)] sm:text-2xl lg:text-3xl`}
-        >
-          <p>
-            Jeg leverer applikasjoner og nettsider
-            <br />
-            tilpasset dine unike behov.
-          </p>
-        </div>
-      </section>
-    </InView>
+        </span>
+        <br />
+        med <span className="text-sky-300">full</span>{" "}
+        <span className="bg-gradient-to-br from-sky-300 to-blue-500 bg-clip-text text-transparent">
+          kontroll.
+        </span>
+      </div>
+      <div
+        className={`${
+          inView ? "translate-0 opacity-100" : "translate-x-[25vw] opacity-0"
+        } mt-8 transform whitespace-nowrap text-center text-lg font-thin italic duration-[1200ms] ease-[cubic-bezier(0,0,0.5,1.5)] sm:text-2xl lg:text-3xl`}
+      >
+        <p>
+          Jeg leverer applikasjoner og nettsider
+          <br />
+          tilpasset dine unike behov.
+        </p>
+      </div>
+    </section>
   );
 };
 
--- src/components/landing-page/LightningFast.tsx
+++ src/components/landing-page/LightningFast.tsx
@@ -1,49 +1,47 @@
-'use client'
-import { InView, useInView } from 'react-intersection-observer';
+"use client";
+import { useInView } from "react-intersection-observer";
 
 const LightningFast = () => {
   const { ref, inView } = useInView({
     root: null,
-    rootMargin: '0px',
+    rootMargin: "0px",
     threshold: 0.5,
   });
 
   return (
-    <InView>
-      <section
-        ref={ref}
-        className={`max-w-screen flex max-h-screen min-h-screen snap-center flex-col items-center justify-center gap-8`}
+    <section
+      ref={ref}
+      className={`max-w-screen flex max-h-screen min-h-screen snap-center flex-col items-center justify-center gap-8`}
+    >
+      <div
+        className={`${
+          inView ? "animate-lightning" : "invisible scale-x-0"
+        } transform-gpu bg-gradient-to-br from-sky-300 to-blue-700 bg-clip-text text-center text-6xl font-black uppercase text-transparent transition-transform sm:block`}
       >
-        <div
-          className={`${
-            inView ? 'animate-lightning' : 'invisible scale-x-0'
-          } transform-gpu bg-gradient-to-br from-sky-300 to-blue-700 bg-clip-text text-center text-6xl font-black uppercase text-transparent transition-transform sm:block`}
-        >
-          Lynrask
-          <br className="sm:hidden" /> ytelse
-        </div>
-        <p
-          className={`${
-            inView
-              ? 'opacity-100 delay-[1900ms] duration-[400ms] ease-in'
-              : 'opacity-0'
-          } text-center text-xl font-thin italic sm:text-2xl lg:text-3xl`}
-        >
-          Jeg kan ta deg fra start til slutt
-          <br />-{' '}
-          <span className="bg-gradient-to-br from-sky-300 to-blue-400 bg-clip-text px-1 font-normal text-transparent">
-            backend
-          </span>{' '}
-          til{' '}
-          <span className="bg-gradient-to-br from-blue-400 to-blue-500 bg-clip-text px-1 font-normal text-transparent">
-            frontend
-          </span>{' '}
-          -
-          <br />
-          for best mulig sluttresultat
-        </p>
-      </section>
-    </InView>
+        Lynrask
+        <br className="sm:hidden" /> ytelse
+      </div>
+      <p
+        className={`${
+          inView
+            ? "opacity-100 delay-[1900ms] duration-[400ms] ease-in"
+            : "opacity-0"
+        } text-center text-xl font-thin italic sm:text-2xl lg:text-3xl`}
+      >
+        Jeg kan ta deg fra start til slutt
+        <br />-{" "}
+        <span className="bg-gradient-to-br from-sky-300 to-blue-400 bg-clip-text px-1 font-normal text-transparent">
+          backend
+        </span>{" "}
+        til{" "}
+        <span className="bg-gradient-to-br from-blue-400 to-blue-500 bg-clip-text px-1 font-normal text-transparent">
+          frontend
+        </span>{" "}
+        -
+        <br />
+        for best mulig sluttresultat
+      </p>
+    </section>
   );
 };
 
