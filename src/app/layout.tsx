e989c8d89cb00a90f9b44a14f0a98312903fbc1f
Daniel Hjartland <squashical@gmail.com>
2023-05-02 07:35:33 +0200

    Add analytics components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import NavBar from "@/components/NavBar/NavBar";
-import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
+import "./globals.css";
+import NavBar from "@/components/NavBar/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className="bg-zinc-1000 h-full text-zinc-300">
         <NavBar />
         <main className="h-full">{children}</main>
+        <Analytics />
       </body>
     </html>
   );
