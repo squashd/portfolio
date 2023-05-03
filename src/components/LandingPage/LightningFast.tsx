28d299c079d8e6518d1eb84f229538031fcf0766
Daniel Hjartland <squashical@gmail.com>
2023-05-03 09:06:19 +0200

    Change lightning fast component to be added to project page

--- src/components/LandingPage/LightningFast.tsx
+++ src/components/LandingPage/LightningFast.tsx
@@ -11,12 +11,12 @@ const LightningFast = () => {
   return (
     <section
       ref={ref}
-      className={`flex h-full w-screen snap-center flex-col items-center justify-center gap-8`}
+      className="my-12 flex h-full snap-center flex-col items-center justify-center gap-8"
     >
       <div
         className={`${
           inView ? "animate-lightning" : "invisible"
-        } absolute w-full bg-gradient-to-br from-sky-300 to-blue-700 bg-clip-text text-center text-6xl font-black uppercase text-transparent transition-transform sm:block`}
+        } w-full bg-gradient-to-br from-sky-300 to-blue-700 bg-clip-text text-center text-6xl font-black uppercase text-transparent transition-transform sm:block`}
       >
         Lynrask
         <br className="sm:hidden" /> ytelse
